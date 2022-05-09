efab92347540dab43b14ddc8baa7301f14236e3d
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-09 11:15:28 -0500

    Models for Admin and User

---
+++ Admin.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Admin extends Model {}
+
+Admin.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // Doctor, tech-admin, receptionist
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // Admin Name
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // Dates/time (Should be able to pass in both date and time)
+    // https://sebhastian.com/sequelize-date-format/
+    appointment_date: {
+      type: DataTypes.DATE,
+    },
+    // Who they saw
+    user_name: {
+        type: DataTypes.STRING,
+    },
+    // Cost of appointment
+    cost: {
+      type: DataTypes.FLOAT,
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'admin',
+  }
+);
+
+module.exports = Admin;
---
+++ User.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class User extends Model {}
+
+User.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        // User Name
+        name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // Dates/time (Should be able to pass in both date and time)
+        // https://sebhastian.com/sequelize-date-format/
+        appointment_date: {
+            type: DataTypes.DATE,
+            allowNull: false,
+        },
+        // Who was seen
+        admin_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        // Cost of appointment
+        cost: {
+            type: DataTypes.FLOAT,
+            allowNull: false,
+        },
+
+    },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'user',
+  }
+);
+
+module.exports = User;
