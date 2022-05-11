a2aff99a79a21e2020b4cf3d86c5690f976d5f8e
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-11 09:44:12 -0500

    Added linking in index.js for Models for User, Appointment, and Admin. Code runs as it ran before.

--- models/index.js
+++ models/index.js
@@ -6,19 +6,19 @@ const Appointment = require('./Appointment');
 const User = require('./User');
 
 // These are linked by when admin and user interacted, denoted by the Model Appointments
-// Admin.belongsToMany(User, {
-//   through: {
-//     model: Appointment,
-//     unique: false
-//   },
-// });
+Admin.belongsToMany(User, {
+  through: {
+    model: Appointment,
+    unique: false
+  },
+});
 
-// User.belongsToMany(Admin, {
-//   through: {
-//     model: Appointment,
-//     unique: false
-//   },
-// });
+User.belongsToMany(Admin, {
+  through: {
+    model: Appointment,
+    unique: false
+  },
+});
 
 
 
