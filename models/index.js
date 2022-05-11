473a94ec9ea8f690472ff1486f785efad8422809
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-10 20:40:32 -0500

    should run now

--- models/index.js
+++ models/index.js
@@ -1,32 +1,32 @@
 const Admin = require('./Admin');
-const Appointment = require('./Appointment');
-const Category = require('./Category');
-const Pharma = require('./Pharma');
-const Scheduler = require('./Scheduler');
+// const Appointment = require('./Appointment');
+// const Category = require('./Category');
+// const Pharma = require('./Pharma');
+// const Scheduler = require('./Scheduler');
 const User = require('./User');
 
 // These are linked by when admin and user interacted, denoted by the Model Appointments
-Admin.belongsToMany(User, {
-  through: {
-    model: Appointment,
-    unique: false
-  },
-});
+// Admin.belongsToMany(User, {
+//   through: {
+//     model: Appointment,
+//     unique: false
+//   },
+// });
 
-User.belongsToMany(Admin, {
-  through: {
-    model: Appointment,
-    unique: false
-  },
-});
+// User.belongsToMany(Admin, {
+//   through: {
+//     model: Appointment,
+//     unique: false
+//   },
+// });
 
 
 
 module.exports = { 
   Admin,
-  Appointment,
-  Category,
-  Pharma,
-  Scheduler,
+  // Appointment,
+  // Category,
+  // Pharma,
+  // Scheduler,
   User};
 
