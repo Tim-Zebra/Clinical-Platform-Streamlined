8773b320d1411057efba0f77f7ed67482702ddd2
Stephen Kelly <sbk13@my.fsu.edu>
2022-05-10 22:45:57 -0400

    login handler solved

--- controllers/index.js
+++ controllers/index.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const dashboardRoutes = require('./dashboardRoutes');
 
+router.use('/dashboard', dashboardRoutes);
 router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
