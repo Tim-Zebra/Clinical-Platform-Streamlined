e49e38131f12013b0dc25f6cb20375da2bd3ad5f
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-12 09:27:21 -0500

    able to pull user data specifically through user id rather than use the search bar

--- controllers/dashboardUserRoutes.js
+++ controllers/dashboardUserRoutes.js
@@ -4,18 +4,12 @@ const withAuth = require('../utils/auth');
 
 // The `/dashboard/user` endpoint
 
-// Leave this for testing purposes. Will use /:id when deploying
-router.get('/', async (req, res) => {
-  res.render('user-main', {
-    layout: 'dashboard',
-  });
-});
-
 // Routes to user at specific ID
-router.get('/:id', async (req, res) => {
+router.get('/', async (req, res) => {
 // gets all associated appointment times, and the associated admins with those times
+req.params.session_id = 1;
 try {
-    const userData = await User.findByPk(req.params.id, {
+    const userData = await User.findByPk(req.params.session_id, {
       attributes: {
         exclude: ['email', 'password'],
       },
@@ -33,15 +27,15 @@ try {
     });
 
     // Uncomment to see admin json response in Insomnia
-    // res.json(userData);
+    res.json(userData);
 
     // Comment out res.render if sending multiple requests to Insomnia
     // Passes post and session status to mustache
-    res.render('user-main', {
-      layout: 'dashboard',
-      userData,
-      // logged_in: req.session.loggedIn
-    });
+    // res.render('user-main', {
+    //   layout: 'dashboard',
+    //   userData,
+    //   // logged_in: req.session.loggedIn
+    // });
   } catch (err) {
     res.status(500).json(err);
   }
