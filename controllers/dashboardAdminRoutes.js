3f455f0121c06f7691d89c345c881d74db9dc600
Stephen Kelly <sbk13@my.fsu.edu>
2022-05-12 13:36:08 -0400

    hello

--- controllers/dashboardAdminRoutes.js
+++ controllers/dashboardAdminRoutes.js
@@ -40,4 +40,99 @@ try {
   }
 });
 
+router.get('/appointments', withAuthAdmin, async (req, res) => {
+  req.session.admin_id = 2;
+  try {
+      const adminData = await Admin.findByPk(req.session.admin_id, {
+        attributes: {
+          exclude: ['email', 'password'],
+        },
+        include: [
+          {
+            model: Appointment,
+            include: {
+              model: User,
+                attributes: {
+                  exclude: ['email', 'password'],
+                },
+            },
+          },
+        ],
+      });
+      res.render('admin-appointments', {
+        layout: 'dashboard',
+        adminData,
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+  router.get('/', withAuthAdmin, async (req, res) => {
+    // gets all associated appointment times, and the associated users with those times
+    req.session.admin_id = 2;
+    try {
+        const adminData = await Admin.findByPk(req.session.admin_id, {
+          attributes: {
+            exclude: ['email', 'password'],
+          },
+          include: [
+            {
+              model: Appointment,
+              include: {
+                model: User,
+                  attributes: {
+                    exclude: ['email', 'password'],
+                  },
+              },
+            },
+          ],
+        });
+    
+        // Uncomment to see admin json response in Insomnia
+        // res.json(adminData);
+    
+        // Uncomment out res.render if sending multiple requests to Insomnia
+        // Passes post and session status to mustache
+        res.render('admin-main', {
+          layout: 'dashboard',
+          adminData,
+        });
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    });
+    
+    router.get('/patients', withAuthAdmin, async (req, res) => {
+      req.session.admin_id = 2;
+      try {
+          const adminData = await Admin.findByPk(req.session.admin_id, {
+            attributes: {
+              exclude: ['email', 'password'],
+            },
+            include: [
+              {
+                model: Appointment,
+                include: {
+                  model: User,
+                    attributes: {
+                      exclude: ['email', 'password'],
+                    },
+                },
+              },
+            ],
+          });
+
+          console.log (adminData);
+          res.render('admin-patients', {
+            layout: 'dashboard',
+            adminData,
+          });
+        } catch (err) {
+          res.status(500).json(err);
+        }
+      });
+  
+
+
 module.exports = router;
