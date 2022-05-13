e87fa7a26634f684519153657c76e21ae47835f7
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-12 20:51:58 -0500

    cleaned out old/commented code in logout js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,25 +1,6 @@
 // Redirects to the landing page, which destroys the session upon arrival
 const logout = async () => document.location.replace('/');
 
-
 document
   .getElementById('logout-btn')
   .addEventListener('click', logout);
-
-
-// Left other info in for scaling app in future (incase we want a different landing page)
-  
-  // const response = await fetch('/api/users/logout', {
-  //   method: 'POST',
-  //   headers: { 'Content-Type': 'application/json' },
-  // });
-
-  // if (response.ok) {
-  //   alert('You are logged out.');
-    // document.location.replace('/');
-  // } else {
-  //   alert('Failed to log out. No user is logged in.');
-  // }
-// };
-
-// document.querySelector('#logout').addEventListener('click', logout);
