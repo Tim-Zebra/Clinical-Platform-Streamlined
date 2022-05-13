228dd2a1d9ef04e473919ba1e4a410d63707cedd
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-13 02:15:29 -0500

    might have fixed double submit.

--- public/js/user-main.js
+++ public/js/user-main.js
@@ -1,35 +1,36 @@
-const loginFormHandler = async (event) => {
-    event.preventDefault();
-  
+var stop = false;
+
+var apptFormHandler = async (event) => {
+  if (stop === true) {
+    stop = false;
+    return;
+  }  else {
+    stop = true;
+  }
+  event.preventDefault();
     // Collect values from the login form
    const date = document.querySelector('#date').value.trim();
    const start_time = document.querySelector('#time2').value.trim();
    const end_time= document.querySelector('#time3').value.trim();
-   var pname = document.querySelector('#pname').value.trim();
    var dname = document.querySelector('#dname').value.trim();
-
-if (pname === "Roger") {
-    var user_id = 1;
-}   else {var user_id=2;}
-
 
 if (dname === "Arthur") {
     var admin_id = 1;
 } else {var admin_id = 2};
 
   
-    if (date && user_id) {
+    if (date && start_time && end_time) {
       // Send a POST request to the API endpoint
       const response = await fetch('/api/users/createAppt', {
   
         method: 'POST',
-        body: JSON.stringify({ date,start_time,end_time,user_id,admin_id }),
+        body: JSON.stringify({ date,start_time,end_time, admin_id }),
         headers: { 'Content-Type': 'application/json' },
       });
-  
+console.log('\x1b[36m', '\n\n----------------This happended-------------------\n\n', '\x1b[37m');
       if (response.ok) {
         // If successful, redirect the browser to the profile page
-        document.location.replace('/dashboard/user/appointments');
+        document.location.replace('/dashboard/user/Appointments');
       } else {
         alert("Login not successful, plesese try again.");
       }
@@ -38,5 +39,5 @@ if (dname === "Arthur") {
 
 
   document
-  .querySelector('.margin-left-user-main-btn')
-  .addEventListener('submit', loginFormHandler);
+  .querySelector('.sbmt')
+  .addEventListener('submit', apptFormHandler);
