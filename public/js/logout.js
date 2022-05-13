a79ae17391c4737d2a5f8eccb14dd4368dba3ff8
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-12 20:51:28 -0500

    Able to log out, cursor appears has finger when hovering over the <a> element that is our log out button

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,35 +1,10 @@
-const logout = async () => {
 // Redirects to the landing page, which destroys the session upon arrival
-document.location.replace('/');
+const logout = async () => document.location.replace('/');
 
-const adminFormHandler = async (event) => {
-  event.preventDefault();
-
-  // Collect values from the login form
-  const email = document.querySelector('#doctor').value.trim();
-  const password = document.querySelector('#date').value.trim();
-
-  if (email && password) {
-    // Send a POST request to the API endpoint
-    const response = await fetch('/api/admin/login', {
-
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (response.ok) {
-      // If successful, redirect the browser to the profile page
-      document.location.replace('/dashboard/admin');
-    } else {
-      alert("Login not successful, plesese try again.");
-    }
-  }
-};
 
 document
-  .querySelector('.cat')
-  .addEventListener('submit', adminFormHandler);
+  .getElementById('logout-btn')
+  .addEventListener('click', logout);
 
 
 // Left other info in for scaling app in future (incase we want a different landing page)
@@ -45,6 +20,6 @@ document
   // } else {
   //   alert('Failed to log out. No user is logged in.');
   // }
-};
+// };
 
 // document.querySelector('#logout').addEventListener('click', logout);
