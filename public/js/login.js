b909572cdddb3b44dc48c95b227dc11e1e6152d1
TZebra <100721290+Tim-Zebra@users.noreply.github.com>
2022-05-12 20:52:23 -0500

    cleaned up login.js by removing console logs

--- public/js/login.js
+++ public/js/login.js
@@ -16,7 +16,6 @@ const loginFormHandler = async (event) => {
       body: JSON.stringify({ email, password }),
       headers: { 'Content-Type': 'application/json' },
     });
-console.log(response);
 
     if (response.ok) {
       // If successful, redirect the browser to the profile page
